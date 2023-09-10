bb85d4ca3fec995cd183119bd5c7ccc1de80c5e8
Tyler Mayoff <tyler@tylermayoff.com>
2023-09-10 12:35:46 -0400

    Added sprites and spawning

--- src/token.rs
+++ src/token.rs
@@ -1,26 +1,43 @@
 use bevy::prelude::*;
 
+use crate::cell::HexCoord;
+
 #[derive(Event)]
 pub enum TokenEvent {
     Spawn,
 }
 
 #[derive(Component)]
-pub struct Token {}
+pub struct Token {
+    pos: HexCoord,
+}
 
 impl Token {
-    fn new(commands: &mut Commands) -> Self {
+    fn new(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Self {
         commands.spawn(SpriteBundle {
+            texture: asset_server.load("sprites/shield-sword.png"),
+            transform: Transform::from_xyz(0.0, 0.0, 0.1),
+            sprite: Sprite {
+                custom_size: Some(Vec2 { x: 55.0, y: 55.0 }),
+                ..Default::default()
+            },
             ..Default::default()
         });
-        Self {}
+
+        Self {
+            pos: HexCoord { q: 0, r: 0 },
+        }
     }
 }
 
-pub fn on_token_event(mut commands: Commands, mut event_reader: EventReader<TokenEvent>) {
+pub fn on_token_event(
+    mut event_reader: EventReader<TokenEvent>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
     for e in event_reader.iter() {
         match e {
-            TokenEvent::Spawn => Token::new(&mut commands),
+            TokenEvent::Spawn => Token::new(&mut commands, &asset_server),
         };
     }
 }
