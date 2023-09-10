9b1aa7ca85bfe1dd43f5ffd99105ba86f3254311
Tyler Mayoff <tyler@tylermayoff.com>
2023-09-10 11:53:56 -0400

    line + box drawing

--- src/cell.rs
+++ src/cell.rs
@@ -1,5 +1,8 @@
+use std::ops;
+
 use crate::grid::*;
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{math::vec4, prelude::*, sprite::MaterialMesh2dBundle};
+use bevy_egui::egui::lerp;
 use bevy_mod_picking::{prelude::*, PickableBundle};
 
 lazy_static! {
@@ -18,16 +21,65 @@ pub enum CellEvent {
     Over(Entity),
 }
 
+#[derive(PartialEq, Eq, Hash)]
 pub struct HexCoord {
     pub q: i32,
     pub r: i32,
+}
+
+pub struct FractionalHexCoord {
+    pub q: f32,
+    pub r: f32,
+}
+
+impl HexCoord {
+    pub fn lerp(a: &HexCoord, b: &HexCoord, t: f32) -> HexCoord {
+        Self::round(FractionalHexCoord {
+            q: lerp(a.q as f32..=b.q as f32, t),
+            r: lerp(a.r as f32..=b.r as f32, t),
+        })
+    }
+
+    fn round(coord: FractionalHexCoord) -> HexCoord {
+        let mut q = (coord.q).round();
+        let mut r = (coord.r).round();
+        let s = (-coord.q - coord.r).round();
+
+        let q_diff = (q - coord.q).abs();
+        let r_diff = (r - coord.r).abs();
+        let s_diff = (s - (-coord.q - coord.r)).abs();
+
+        if q_diff > r_diff && q_diff > s_diff {
+            q = -r - s;
+        } else if r_diff > s_diff {
+            r = -q - s
+        } else {
+            // s = -q - r
+        }
+
+        HexCoord {
+            q: q as i32,
+            r: r as i32,
+        }
+    }
+}
+
+impl ops::Sub<&HexCoord> for &HexCoord {
+    type Output = HexCoord;
+
+    fn sub(self, rhs: &HexCoord) -> Self::Output {
+        HexCoord {
+            q: self.q - rhs.q,
+            r: self.r - rhs.r,
+        }
+    }
 }
 
 #[derive(Component)]
 pub struct Cell {
     _size: f32,
     pub pos: HexCoord,
-    pub color_managed: bool,
+    pub color: Color,
 }
 
 impl Cell {
@@ -41,8 +93,8 @@ impl Cell {
     ) -> Entity {
         let c = Cell {
             _size: size,
-            color_managed: false,
             pos,
+            color: *HEX_COLOR,
         };
 
         let mesh = MaterialMesh2dBundle {
@@ -77,10 +129,8 @@ fn on_hover_enter(
 ) {
     // Update the color of the cell
     if let Ok((cell, mat)) = cell_q.get(event.target) {
-        if !cell.color_managed {
-            let material = materials.get_mut(mat).unwrap();
-            material.color = *HEX_HOVER_COLOR;
-        }
+        let material = materials.get_mut(mat).unwrap();
+        material.color = cell.color + vec4(-0.2, -0.2, -0.2, 0.0);
     }
 
     cell_event.send(CellEvent::Over(event.target));
@@ -93,10 +143,8 @@ fn on_hover_out(
 ) {
     // Revert the color of the cell
     if let Ok((cell, mat)) = cell_q.get(event.target) {
-        if !cell.color_managed {
-            let material = materials.get_mut(mat).unwrap();
-            material.color = *HEX_COLOR;
-        }
+        let material = materials.get_mut(mat).unwrap();
+        material.color = cell.color;
     }
 }
 
--- src/grid.rs
+++ src/grid.rs
@@ -1,3 +1,5 @@
+use std::{cmp::max, collections::HashMap};
+
 use crate::cell::{Cell, HexCoord};
 
 use bevy::{prelude::*, sprite::ColorMaterial};
@@ -24,7 +26,7 @@ lazy_static! {
 #[derive(Component)]
 pub struct Grid {
     pub size: i32,
-    pub cells: Vec<Vec<Entity>>,
+    pub cells: HashMap<HexCoord, Entity>,
 }
 
 impl Grid {
@@ -34,26 +36,27 @@ impl Grid {
         meshes: &mut ResMut<Assets<Mesh>>,
         materials: &mut ResMut<Assets<ColorMaterial>>,
     ) {
-        let horizontal_spacing: f32 = (*HEX_GRID_HORIZONTAL_OFFSET * HEX_SIZE) + HEX_SPACING;
-        let vertical_spacing: f32 = 1.5 * HEX_SIZE + HEX_SPACING;
-
         let mut grid = Grid {
             size,
-            cells: Vec::new(),
+            cells: HashMap::new(),
         };
 
-        let mut temp_cells = vec![vec![None; size as usize]; size as usize];
+        let left: i32 = -size / 2;
+        let right: i32 = size / 2;
+        let top: i32 = -size / 2;
+        let bottom: i32 = size / 2;
 
-        for r in 0..size {
-            for q in 0..size {
-                let x_offset = if r % 2 == 0 {
-                    horizontal_spacing / 2.0
-                } else {
-                    0.0
-                };
+        let f0: f32 = 3.0_f32.sqrt();
+        let f1: f32 = 3.0_f32.sqrt() / 2.0;
+        let f2: f32 = 0.0;
+        let f3: f32 = 3.0 / 2.0;
 
-                let x_pos = (q - (size / 2)) as f32 * horizontal_spacing + x_offset;
-                let y_pos = (r - (size / 2)) as f32 * vertical_spacing;
+        for r in top..=bottom {
+            let offset_r = (r as f32 / 2.0).floor() as i32;
+
+            for q in (left - offset_r)..=(right - offset_r) {
+                let x_pos = (f0 * q as f32 + f1 * r as f32) * (HEX_SIZE + HEX_SPACING);
+                let y_pos = (f2 * q as f32 + f3 * r as f32) * (HEX_SIZE + HEX_SPACING);
 
                 let id = Cell::create(
                     Vec2::new(x_pos, y_pos),
@@ -64,78 +67,54 @@ impl Grid {
                     materials,
                 );
 
-                let index_q = q as f32 + (r as f32 / 2.0).floor();
-                temp_cells[r as usize][(index_q) as usize] = Some(id);
+                grid.cells.insert(HexCoord { q, r }, id);
             }
-        }
-
-        for row in temp_cells {
-            let mut row_vec = Vec::new();
-            for cell in row {
-                row_vec.push(cell.unwrap());
-            }
-            grid.cells.push(row_vec);
         }
 
         commands.spawn(grid);
+    }
+
+    pub fn get_cells_in_box(&self, start: &HexCoord, end: &HexCoord) -> Vec<Entity> {
+        let mut cells = vec![
+            *self.cells.get(start).unwrap(),
+            *self.cells.get(end).unwrap(),
+        ];
+
+        let c1 = HexCoord {
+            q: start.q,
+            r: end.r,
+        };
+
+        let c2 = HexCoord {
+            q: end.q,
+            r: start.r,
+        };
+
+        cells.append(&mut self.get_cells_in_line(start, &c1));
+        cells.append(&mut self.get_cells_in_line(start, &c2));
+        cells.append(&mut self.get_cells_in_line(&c1, end));
+        cells.append(&mut self.get_cells_in_line(&c2, end));
+
+        cells
     }
 
     pub fn get_cells_in_line(&self, start: &HexCoord, end: &HexCoord) -> Vec<Entity> {
         let mut cells = Vec::new();
 
         let n = Self::axial_distance(start, end);
+        let step = 1.0 / max(n, 1) as f32;
         for i in 0..n {
-            let lerped = Self::cube_lerp(start, end, 1.0 / n as f32 * i as f32);
-            let rounded = Self::cube_round(lerped.q as f32, lerped.r as f32);
-            cells.push(self.cells[rounded.q as usize][rounded.r as usize]);
+            let lerped = HexCoord::lerp(start, end, step * i as f32);
+            cells.push(self.cells.get(&lerped).unwrap().to_owned());
         }
+
+        cells.push(self.cells.get(end).unwrap().to_owned());
 
         cells
     }
 
-    fn lerp(a: f32, b: f32, t: f32) -> f32 {
-        a + (b - a) * t
-    }
-
-    fn cube_lerp(a: &HexCoord, b: &HexCoord, t: f32) -> HexCoord {
-        HexCoord {
-            q: Grid::lerp(a.q as f32, b.q as f32, t).round() as i32,
-            r: Grid::lerp(a.r as f32, b.r as f32, t).round() as i32,
-        }
-    }
-
-    fn axial_subtract(a: &HexCoord, b: &HexCoord) -> HexCoord {
-        HexCoord {
-            q: a.q - b.q,
-            r: a.r - b.r,
-        }
-    }
-
     fn axial_distance(a: &HexCoord, b: &HexCoord) -> i32 {
-        let vec = Self::axial_subtract(a, b);
+        let vec = a - b;
         (vec.q.abs() + vec.r.abs() + (vec.q + vec.r).abs()) / 2
-    }
-
-    fn cube_round(frac_q: f32, frac_r: f32) -> HexCoord {
-        let mut q = (frac_q).round();
-        let mut r = (frac_r).round();
-        let mut s = (-frac_q - frac_r).round();
-
-        let q_diff = (q - frac_q).abs();
-        let r_diff = (r - frac_r).abs();
-        let s_diff = (s - (-frac_q - frac_r)).abs();
-
-        if q_diff > r_diff && q_diff > s_diff {
-            q = -r - s;
-        } else if r_diff > s_diff {
-            r = -q - s
-        } else {
-            s = -q - r
-        }
-
-        HexCoord {
-            q: q as i32,
-            r: r as i32,
-        }
     }
 }
